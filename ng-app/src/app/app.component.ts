44ce6494dfb4d77199f43c8e282d79d5829b9c37
Greg Stanley <24586962+gregstanley@users.noreply.github.com>
2024-12-19 17:23:16 +0000

    Fix index files and make Angular slightly more interesting

--- ng-app/src/app/app.component.ts
+++ ng-app/src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterLink, RouterOutlet } from '@angular/router';
+import { TestComponent } from './test/test.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, RouterLink],
+  imports: [RouterOutlet, RouterLink, TestComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
--- ng-app/src/app/app.routes.ts
+++ ng-app/src/app/app.routes.ts
@@ -1,9 +1,3 @@
 import { Routes } from '@angular/router';
-import { TestComponent } from './test/test.component';
 
-export const routes: Routes = [
-  {
-    path: 'test',
-    component: TestComponent,
-  },
-];
+export const routes: Routes = [];
