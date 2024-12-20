8ad8c364562a4cf88a15550b5be10a5ba0da3e4e
Greg Stanley <24586962+gregstanley@users.noreply.github.com>
2024-12-20 12:13:40 +0000

    Start adding packages and try Tailwind

--- ng-app/src/app/app.component.ts
+++ ng-app/src/app/app.component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
-import { RouterLink, RouterOutlet } from '@angular/router';
+import { RouterOutlet } from '@angular/router';
 import { TestComponent } from './test/test.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, RouterLink, TestComponent],
+  imports: [RouterOutlet, TestComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
